1749158f5a870eabce7cf24e86b0abcca5b07b5f
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-23 19:18:45 +0300

    add  route to blog post

---
+++ blogging-platform/backend/routes/postsRoute.js
@@ -0,0 +1,95 @@
+import express, { request, response } from 'express';
+import { Post } from '../modals/postsModal';
+
+const router = express.Router();
+// route to add new post
+router.post('/', async (request,response) => {
+    try{
+        if(
+            !request.body.title || 
+            !request.body.author || 
+            !request.body.content ||
+            !request.body.catagory ){
+            return response.status(400).send({ message: 'send all required fields: title,post,catagory'});
+        }
+        const newPost = {
+            title:request.body.title,
+            content:request.body.content,
+            author:request.body.author,
+            tags:request.body.tags,
+            Image:request.body.Image,
+            catagory:request.body.catagory,
+            status: 'posted',
+        }
+        const post = await Post.create(newPost);
+        return response.status(201).send(post);
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message : error.message});
+    }
+});
+// route to get all post
+router.get('/',async (request,response)=>{
+    try{
+        const posts = await Post.find({});
+        return response.status(200).json({
+            count: posts.length,
+            data:posts
+        }
+        );
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+// route to get post by id
+router.get('/:id', async (request,response)=>{
+    try{
+        const {id} = request.params;
+        const post = await Posts.findById(id);
+        return response.status(200).json({
+            data:post
+        }
+        );
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+// route to Update post
+router.put('/:id', async (request,response)=>{
+    try{
+        if(
+            !request.body.title || 
+            !request.body.author || 
+            !request.body.content ||
+            !request.body.catagory ){
+            return response.status(400).send({ message: 'send all required fields: title,post,catagory'});
+        }
+        const {id} = request.params;
+        const result = await Post.findByIdAndUpdate(id,request.body);
+        if(!result){
+            return response.status(404).json({message: 'Post not found'})
+        }
+        return response.status(200).send({message: 'Post Updated successfuly'})
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+// route to Delete a post
+router.delete('/:id', async (request,response)=>{
+    try{
+        const {id} = request.params;
+        const result = await Book.findByIdAndDelete(id);
+        if(!result){
+            return response.status(404).json({message: 'Post not found'})
+        }
+        return response.status(200).send({message: 'Post Deleted successfuly'})
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+export default router;
