c212295c7d2870c803d8f0f5bfb380dd30880945
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-23 17:13:42 +0300

    connected to mongodb

---
+++ blogging-platform/backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'
+import dotenv from 'dotenv'; 
+
+dotenv.config()
+const app = express();
+app.use(express.json());
+
+// middleware to handle cors policy
+// allow all origins for now
+
+app.use(cors())
+
+// define routes
+app.get('/',(req,res)=>{
+    return res.status(234).send('welcome to our awesome blogging site')
+})
+
+// lets connect to db
+mongoose.connect(process.env.DATABASE_URL).then(()=>{
+    console.log('App connected to database');
+    app.listen(5555 || process.env.PORT ,()=>{
+        console.log(`App is listening to Port : ${process.env.PORT}`)
+    })
+}).catch((error)=> {
+        console.log(error)
+})
