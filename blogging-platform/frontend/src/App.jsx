c250025ee9bb44d4495ff6b0f352400ef1e41f34
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-24 14:48:22 +0300

    setup routes for frontend

--- blogging-platform/frontend/src/App.jsx
+++ blogging-platform/frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+// Pages
+import Home from './Pages/Posts/Home'
+import CreatePosts from './Pages/Posts/CreatePosts'
+import ShowPosts from './Pages/Posts/ShowPosts'
+import EditPosts from './Pages/Posts/EditPosts'
+import DeletePosts from './Pages/Posts/DeletePosts'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/posts/create' element={<CreatePosts />} />
+      <Route path='/posts/details/:id' element={<ShowPosts />} />
+      <Route path='/posts/edit/:id' element={<EditPosts />} />
+      <Route path='/posts/delete/:id' element={<DeletePosts />} />
+    </Routes>
   )
 }
 
