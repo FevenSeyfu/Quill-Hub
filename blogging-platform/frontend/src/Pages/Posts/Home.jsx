cdc3b7f55405107fd5e8bea609e40a3167188e1b
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-24 15:08:19 +0300

    fetch from server  to frontend

--- blogging-platform/frontend/src/Pages/Posts/Home.jsx
+++ blogging-platform/frontend/src/Pages/Posts/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Spinner from "../../components/Spinner";
+
 
 const Home = () => {
+   const [loading, setLoading] = useState(false);
+   const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/posts")
+      .then((response) => {
+        setPosts(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div>Home</div>
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div>
+            hello
+        </div>
+      )}
+    </div>
   )
 }
 
