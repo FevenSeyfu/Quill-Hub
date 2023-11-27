e1e58bb8b43b7c5b74e87a197fb26495b6f6edaf
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-27 20:27:44 +0300

    Add grided post listing

--- blogging-platform/frontend/src/components/Home/Main/PostCard.jsx
+++ blogging-platform/frontend/src/components/Home/Main/PostCard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const PostCard = () => {
+const PostCard = ({ post }) => {
+  const { title, author, date, votes, description, imageUrl, tag } = post;
+
   return (
-    <div>PostCard</div>
-  )
-}
+    <div className="post-card">
+      <img src={imageUrl} alt={title} className="post-image" />
+      <div className="post-content">
+        <h3 className="post-title">{title}</h3>
+        <div className="post-meta">
+          <p className="meta-item">{`By: ${author}`}</p>
+          <p className="meta-item">{`Date: ${date}`}</p>
+          <p className="meta-item">{`Votes: ${votes}`}</p>
+        </div>
+        <p className="post-description">{description}</p>
+        <a href="#" className="read-more-link">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default PostCard
+export default PostCard;
