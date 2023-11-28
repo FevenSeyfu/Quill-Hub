2b83574b274213a5b6564f142a2b4fe2b2c534da
FevenSeyfu <fevenseyfu00@gmail.com>
2023-11-28 21:32:36 +0300

    Add login page

--- blogging-platform/frontend/src/Pages/Auth/Login.jsx
+++ blogging-platform/frontend/src/Pages/Auth/Login.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoLogoGoogle } from 'react-icons/io5';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center bg-purple-telemagnet">
+      <div className="max-w-6xl w-5/6 flex rounded-2xl items-center justify-center  bg-soft-white shadow-md ">
+        <div className="w-full px-6 md:w-2/6 mx-8 md:mb-0 ">
+          <div className="flex items-center justify-center mb-6">
+            <img
+              src="https://source.unsplash.com/user/wsanter"
+              alt="Logo"
+              className="w-16 h-16 object-contain"
+            />
+          </div>
+          <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+          <form>
+            <div className="mb-4">
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Username or Email
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-600"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="mt-1 p-2 w-full border rounded-md"
+              />
+            </div>
+            <div className="flex items-center mb-4">
+              <input
+                type="checkbox"
+                id="rememberMe"
+                name="rememberMe"
+                className="mr-2"
+              />
+            <div className="flex flex-row ">
+                <label htmlFor="rememberMe" className="text-sm font-medium text-gray mr-6">Remember me</label>
+                <p className="text-sm text-gray">
+                    <a href="#">Forgot Password?</a>
+                </p>
+            </div>
+            </div>
+            <button
+            type="submit"
+            className="w-full  text-white  bg-soft-orange rounded-2xl py-2 mr-2"
+            >
+            Login
+            </button>
+          </form>
+          <hr className="my-6 border-t border-gray-300" />
+          <div className="text-center">
+            <p className="text-sm font-medium text-gray-600 mb-2">
+              Or Login with
+            </p>
+            <div type=" flex items-center justify-center">
+               <IoLogoGoogle/> Sign in with Google
+            </div>
+          </div>
+          <p>Don't have an accound?
+            <Link to="/login" className="text-soft-orange hover:underline">
+                Register
+            </Link>
+        </p>
+        </div>
+        {/* Right side - Welcome message and illustration */}
+        <div className="hidden md:block w-full md:w-4/6 pt-8  rounded-lg text-soft-white  bg-soft-orange">
+          <div className="text-center">
+            <h2 className="text-3xl font-bold mb-4">Welcome</h2>
+            <p className="text-gray-600 mb-4">
+              Enjoy the benefits of our platform by signing in. Join our
+              community and explore a world of possibilities.
+            </p>
+          </div>
+          <img
+            src="https://source.unsplash.com/user/wsanter"
+            alt="Illustration"
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- blogging-platform/frontend/src/Pages/Auth/Register.jsx
+++ blogging-platform/frontend/src/Pages/Auth/Register.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <form className="bg-white p-8 shadow-md rounded-md">
+        <h2 className="text-2xl font-bold mb-4">Register</h2>
+        {/* Registration form */}
+      </form>
+    </div>
   )
 }
 
