35491268289a971021ffb06596ae42ca1b6831e2
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-04 16:31:14 +0300

    Setup frontend routes for comment

--- blogging-platform/frontend/src/App.jsx
+++ blogging-platform/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Routes, Route , Navigate} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-// Pages
+// post Pages
 import Home from './Pages/Posts/Home'
 import CreatePosts from './Pages/Posts/CreatePosts'
 import ShowPost from './Pages/Posts/ShowPost'
@@ -14,7 +14,12 @@ import Login from './Pages/Auth/Login'
 import Register from './Pages/Auth/Register'
 import UserProfile from './Pages/Auth/UserProfile'
 
-
+// comments Page
+import CreateComment from './Pages/Comments/CreateComment'
+import DeleteComment from './Pages/Comments/DeleteComment'
+import EditComment from './Pages/Comments/EditComment'
+import LikeComment from './Pages/Comments/LikeComment'
+import ShowComments from './Pages/Comments/ShowComments'
 
 const App = () => {
   const isUserSignedIn = true;
@@ -27,9 +32,17 @@ const App = () => {
         <Route path='/posts/details/:id' element={<ShowPost />} />
         <Route path='/posts/edit/:id' element={<EditPost />} />
         <Route path='/posts/delete/:id' element={<DeletePost />} />
+          {/* for user registration */}
         <Route path="/users/" element={<Register/>} />
         <Route path="/users/login" element={<Login/>} />
         <Route path="/users/profile" element={<UserProfile/>} />
+
+        {/* for commenting system */}
+        <Route path='/posts/:postId/comments/create' element={<CreateComment />} />
+        <Route path='/posts/:postId/comments' element={<ShowComments />} />
+        <Route path='/posts/:postId/comments/edit/:commentId' element={<EditComment />} />
+        <Route path='/posts/:postId/comments/delete/:commentId' element={<DeleteComment />} />
+        <Route path='/posts/:postId/comments/:commentId/like' element={<LikeComment />} />
       </Routes>
       <ToastContainer />
     </>
---
+++ blogging-platform/frontend/src/Pages/Comments/LikeComment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LikeComment = () => {
+  return (
+    <div>LikeComment</div>
+  )
+}
+
+export default LikeComment
