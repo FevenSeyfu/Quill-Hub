d52d95513f303c3d06979fbb11f221e35b142603
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-04 18:37:13 +0300

    Complete create comment component

--- blogging-platform/frontend/src/Pages/Comments/CreateComment.jsx
+++ blogging-platform/frontend/src/Pages/Comments/CreateComment.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createComment } from "../../features/comment/commentSlice";
+import { toast } from "react-toastify";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 const CreateComment = () => {
-  return (
-    <div>CreateComment</div>
-  )
-}
+  const dispatch = useDispatch();
+  const [commentContent, setCommentContent] = useState("");
+  const { user } = useSelector((state) => state.auth);
+  const { posts } = useSelector((state) => state.post);
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
 
-export default CreateComment
+    if (!commentContent.trim()) {
+      toast.error("Please write Comment");
+      return;
+    }
+
+    try {
+      await dispatch(createComment({ post: postId, content: commentContent }));
+      setCommentContent("");
+      posts.id === postId && posts.commentCount++;
+
+      navigate("/posts");
+    } catch (error) {
+      toast.error("Error creating comment:", error.message);
+    }
+  };
+  return (
+    <Modal
+      isOpen={true}
+      contentLabel="Delete Post Modal"
+      className="fixed top-0 left-0 w-full h-full flex justify-center items-center"
+      overlayClassName="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
+    >
+      <div className="bg-white p-8 rounded">
+        <h1 className="text-3xl font-bold mb-4">Add a Comment</h1>
+        <form onSubmit={handleCommentSubmit}>
+          <textarea
+            className="w-full p-2 border rounded"
+            rows="3"
+            placeholder="Write your comment..."
+            value={commentContent}
+            onChange={(e) => setCommentContent(e.target.value)}
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 mt-2 rounded hover:bg-blue-600"
+          >
+            Post Comment
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
+};
+
+export default CreateComment;
--- blogging-platform/frontend/src/components/Home/Main/PostCard.jsx
+++ blogging-platform/frontend/src/components/Home/Main/PostCard.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+import CreateComment from "../../../Pages/Comments/CreateComment";
 // icons
 import { BiSolidShow } from "react-icons/bi";
 import { MdDeleteForever } from "react-icons/md";
@@ -9,15 +10,17 @@ import { TbEdit } from "react-icons/tb";
 import { FaHeart } from "react-icons/fa";
 import { FaCommentDots } from "react-icons/fa";
 
+
 const PostCard = ({ post }) => {
-  const { user } = useSelector((state) => state.auth)
-  const { title, author, createdAt, votesCount, Image, tags,commentCount } = post;
+  const { user } = useSelector((state) => state.auth);
+  const { title, author, createdAt, votesCount, Image, tags, commentCount } =
+    post;
 
   const handleDate = (dateInput) => {
     const date = new Date(dateInput);
-    const formattedDate = date.toISOString().split('T')[0];
-    return formattedDate
-  }
+    const formattedDate = date.toISOString().split("T")[0];
+    return formattedDate;
+  };
 
   const authorName = user.id === author && user.userName;
   return (
@@ -26,10 +29,16 @@ const PostCard = ({ post }) => {
         {tags[0]}
       </div>
       {Image ? (
-        <img className="rounded-lg overflow-hidden items-center h-48 w-full" src={Image} alt={title} />
-      ):(
+        <img
+          className="rounded-lg overflow-hidden items-center h-48 w-full"
+          src={Image}
+          alt={title}
+        />
+      ) : (
         <div className="bg-black rounded-lg overflow-hidden items-center h-48 w-full">
-          <div className="text-white pt-20  text-lg text-center p-4">{title.toUpperCase()}</div>
+          <div className="text-white pt-20  text-lg text-center p-4">
+            {title.toUpperCase()}
+          </div>
         </div>
       )}
       <div className="px-4 py-3">
@@ -38,32 +47,35 @@ const PostCard = ({ post }) => {
       <div className="px-6 py-4">
         <div className="post-meta flex flex-col sm:flex-col">
           <p className="text-gray-500 text-sm mr-2">
-            <b>By: </b>{authorName}
+            <b>By: </b>
+            {authorName}
           </p>
           <p className="text-gray-500 text-sm mr-2">
-            <b>Post Date:</b> {createdAt && (handleDate(createdAt))}
+            <b>Post Date:</b> {createdAt && handleDate(createdAt)}
           </p>
           <p className="flex flex-row items-center text-gray-500 text-md">
-            <FaHeart  className='text-red pr-1 text-lg'/> {':'}
+            <FaHeart className="text-red pr-1 text-lg" /> {":"}
             {votesCount}
           </p>
+        <Link to={`/posts/${post._id}/comments/create/`}>
           <p className="flex flex-row items-center text-gray-500 text-md">
-            <FaCommentDots  className='text-gray pr-1 text-lg'/> {':'}
+            <FaCommentDots className="text-gray pr-1 text-lg" /> {":"}
             {commentCount}
           </p>
+        </Link>
         </div>
       </div>
       <div className="flex flex-row relative justify-between bottom-0  m-2">
-          <Link to={`/posts/details/${post._id}`}>
-              <BiSolidShow className="text-blue font-bold hover:underline text-3xl"/>
-          </Link>
-          <Link to={`/posts/edit/${post._id}`}>
-              <TbEdit className="text-green font-bold hover:underline text-3xl"/>
-          </Link>
-          <Link to={`/posts/delete/${post._id}`}>
-              <MdDeleteForever className="text-red font-bold hover:underline text-3xl"/>
-          </Link>
-        </div>
+        <Link to={`/posts/details/${post._id}`}>
+          <BiSolidShow className="text-blue font-bold hover:underline text-3xl" />
+        </Link>
+        <Link to={`/posts/edit/${post._id}`}>
+          <TbEdit className="text-green font-bold hover:underline text-3xl" />
+        </Link>
+        <Link to={`/posts/delete/${post._id}`}>
+          <MdDeleteForever className="text-red font-bold hover:underline text-3xl" />
+        </Link>
+      </div>
     </div>
   );
 };
