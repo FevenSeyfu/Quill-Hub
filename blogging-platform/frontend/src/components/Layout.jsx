4e61c57d8ce21a25c08a8041c986de7596c38675
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-05 23:24:09 +0300

    amend user profile

--- blogging-platform/frontend/src/components/Layout.jsx
+++ blogging-platform/frontend/src/components/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
       <div className="flex flex-col ml-64">
         <Header headerName={'Explore'} />
         <div className="flex flex-row mt-4">
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow w-80">{children}</main>
           <div className="relative right-2 top-2">
             <RightSideBar />
           </div>
