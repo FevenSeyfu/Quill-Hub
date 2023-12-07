e98bfcf11b3bda1031f9252e15c4b31a60019b58
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-07 18:36:56 +0300

    add end date to sprintschema

--- Project-management-tool/backend/models/sprintModels.js
+++ Project-management-tool/backend/models/sprintModels.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const sprintSchema = new mongoose.Schema({
   name: { type: String, required: true },
   startDate: { type: Date, required: true },
+  endDate: { type: Date }, 
   duration: { type: Number, required: true },
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
 });
