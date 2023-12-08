7e18ce7c11a6422ddfcc4c44547d9347e8d1931b
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-08 15:46:49 +0300

    Update links

--- blogging-platform/frontend/src/components/Home/SideBar.jsx
+++ blogging-platform/frontend/src/components/Home/SideBar.jsx
@@ -5,6 +5,7 @@ import { FaMedium } from "react-icons/fa6";
 import { IoMailOutline,IoChevronDown } from "react-icons/io5";
 import Logo from '../../assets/logo-no-bg.png'
 import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
   const { user } = useSelector((state) => state.auth);
@@ -13,37 +14,37 @@ const SideBar = () => {
     <>
     <div className={`sidebar  text-white fixed left-0 top-0 flex flex-col justify-between bg-purple-telemagnet  w-64 h-full ${sidebarVisible ? 'visible' : 'hidden'} lg:visible`}>
       <div className='flex flex-col justify-around items-center m-2'>
-        <a href="/">
+      <Link to="/">
         <img src={Logo} alt="Logo" className="w-24 h-16"/>
         <h2 className='font-bold text-lg text-gray-soft-white'>QuillHUB</h2>
-        </a>
+      </Link>
       </div>
       <nav className="flex-1 flex flex-col items-start justify-start mt-6 p-4">
-        <a href="/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
+        <Link to="/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
           Home
           <IoChevronDown className="ml-2" />
-        </a>
+        </Link>
         {user ? (
           <>
-            <a href="/posts/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
+            <Link to="/posts/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
               My Stories
               <IoChevronDown className="ml-2" />
-            </a>
-          <a href="/users/profile" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
+            </Link>
+          <Link to="/users/profile" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
               My Profile
               <IoChevronDown className="ml-2" />
-            </a>
+            </Link>
           </>
         ):(
           <>
-            <a href="/users/login/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
+            <Link to="/users/login/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
               Login
               <IoChevronDown className="ml-2" />
-            </a>
-          <a href="/users/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
+            </Link>
+          <Link to="/users/" className="flex items-center mb-4 justify-between w-full hover:underline font-bold text-lg">
               Register
               <IoChevronDown className="ml-2" />
-            </a>
+          </Link>
           </>
         )}
       </nav>
