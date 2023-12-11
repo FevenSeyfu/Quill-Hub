abc96abc1110e735b44265da275520d57fc659a1
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-11 18:25:16 +0300

    add protect to routes

--- Project-management-tool/backend/routes/projectRoutes.js
+++ Project-management-tool/backend/routes/projectRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import {getAllProjects,getProjectById,createProject,updateProject,deleteProject} from '../controllers/projectController.js';
-
+import { protect } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 
-router.get('/', getAllProjects);
-router.get('/:id', getProjectById);
-router.post('/', createProject);
-router.put('/:id', updateProject);
-router.delete('/:id', deleteProject);
+router.get('/',protect, getAllProjects);
+router.get('/:id',protect, getProjectById);
+router.post('/', protect,createProject);
+router.put('/:id',protect, updateProject);
+router.delete('/:id',protect, deleteProject);
 
 export default router;
--- Project-management-tool/backend/routes/sprintRoutes.js
+++ Project-management-tool/backend/routes/sprintRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import {getAllSprints,getSprintById,createSprint,updateSprint,deleteSprint} from '../controllers/sprintController.js';
-
+import { protect } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 
-router.get('/', getAllSprints);
-router.get('/:id', getSprintById);
-router.post('/', createSprint);
-router.put('/:id', updateSprint);
-router.delete('/:id', deleteSprint);
+router.get('/',protect, getAllSprints);
+router.get('/:id',protect, getSprintById);
+router.post('/',protect, createSprint);
+router.put('/:id',protect, updateSprint);
+router.delete('/:id',protect, deleteSprint);
 
 export default router;
