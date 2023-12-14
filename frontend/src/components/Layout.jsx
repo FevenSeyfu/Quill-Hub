536ef212d78c906e0b874cc28b533b39547bae72
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-14 23:37:27 +0300

    update card size

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -10,7 +10,9 @@ const Layout = ({ children,headerName }) => {
       <div className="flex flex-col lg:ml-64">
         <Header headerName={headerName} className="fixed h-10"/>
         <div className="flex flex-row mt-4">
-          <main className="flex-grow w-4/5">{children}</main>
+          <main className="flex-grow w-4/5">
+            {children}
+          </main>
           <RightSideBar className="fixed right-2 top-16 lg:w3/6"/>
         </div>
       </div>
